9632ef335b794dcf6a8662b2ec54b6d710b8fa87
GDISSA <47599833+gdissa@users.noreply.github.com>
2020-12-15 15:28:19 +0100

    Update maps.dart
    Clean

--- apolline-flutter/lib/widgets/maps.dart
+++ apolline-flutter/lib/widgets/maps.dart
@@ -1,9 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
